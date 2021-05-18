f98b45bdeeabcdcd027a32d4afd9301e861f2fd1
Lev Lebedev <levask00@gmail.com>
2021-05-18 19:55:49 +0500

    Initial

---
+++ YeelightMac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YeelightMac
+//
+//  Created by John Greenwood on 18.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YeelightMac/YeelightMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  YeelightMacApp.swift
+//  YeelightMac
+//
+//  Created by John Greenwood on 18.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct YeelightMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
